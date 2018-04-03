55a6433758c0f8fbd9eaf62795ddf3cf753f5e4d
Yu-Jen Chang <juzen2003@gmail.com>
2018-04-03 16:58:30 -0700

    Create signup container and signup route

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import LogInFormContainer from './session_form/login_container_form'
+import LogInFormContainer from './session_form/login_container'
+import SignUpFormContainer from './session_form/signup_container'
 
 
 const App = () => (
   <div>
     <h1>EasyNote App</h1>
     <Route exact path="/login" component={LogInFormContainer} />
+    <Route exact path="/signup" component={SignUpFormContainer} />
   </div>
 );
 
--- frontend/easyNote.jsx
+++ frontend/easyNote.jsx
@@ -11,8 +11,8 @@ document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={store}/>, root);
 
-  // window.getState = store.getState;
-  // window.dispatch = store.dispatch;
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
   // window.login = login;
   // window.logout = logout;
   // window.signup = signup;
