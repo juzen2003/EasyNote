bad1c09ec56e370bd52d490e5ab87a1feeba353d
Yu-Jen Chang <juzen2003@gmail.com>
2018-04-03 11:53:36 -0700

    Update application, users, sessions controller

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,7 +1,22 @@
 class Api::SessionsController < ApplicationController
   def create
+    # Find the user by credential
+    @user = User.find_by_credentials(params[:user][:username],params[:user][:password])
+    # debugger
+    if @user.nil?
+      render json: ['Invalid credentials!'], status: 401
+    else
+      login(@user)
+      render 'api/users/show'
+    end
   end
 
   def destroy
+    if current_user.nil?
+      render json: ['Fail to logout!'], status: 404
+    else
+      logout
+      render json: {}
+    end
   end
 end
