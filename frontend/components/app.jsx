7119d1daee49eec81caa2cb406ec34553dad5698
Yu-Jen Chang <juzen2003@gmail.com>
2018-04-04 00:53:13 -0700

    Add route_util to make Auth & Protected route

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom';
 import LogInFormContainer from './session_form/login_container';
 import SignUpFormContainer from './session_form/signup_container';
 import NavSideBarContainer from './nav_side_bar/nav_side_bar_container';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
 
 
@@ -11,10 +12,10 @@ const App = () => (
     <header>
       <h1>EasyNote App</h1>
     </header>
-    <Route exact path="/" component={LogInFormContainer} />
-    <Route path="/login" component={LogInFormContainer} />
-    <Route path="/signup" component={SignUpFormContainer} />
-    <Route path="/notes" component={NavSideBarContainer} />
+    <AuthRoute exact path="/" component={LogInFormContainer} />
+    <AuthRoute path="/login" component={LogInFormContainer} />
+    <AuthRoute path="/signup" component={SignUpFormContainer} />
+    <ProtectedRoute path="/notes" component={NavSideBarContainer} />
   </div>
 );
 
---
+++ frontend/util/route_util.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Redirect, Route, withRouter } from 'react-router-dom';
+
+const mapStateToProps = state => ({
+  loggedIn: Boolean(state.session.currentUser)
+});
+
+// Build Auth component
+// when logged in, redirect to /notes, else render component
+// this is to avoid logged in user to go to signup/login form again
+const Auth = ({ loggedIn, exact, path, component: Component}) => (
+  <Route
+    exact={exact}
+    path={path}
+    render={props => (
+      loggedIn ? <Redirect to='/notes'/> : <Component {...props}/>
+    )}
+  />
+)
+
+// Build Protected component
+// when logged in, render component, else redirect to signup
+// this is to protect not-logged-in user to directly render '/notes'
+const Protected = ({ loggedIn, path, component: Component}) => (
+  <Route
+    path={path}
+    render={props => (
+      loggedIn ? <Component {...props}/> : <Redirect to='/signup'/>
+    )}
+  />
+)
+
+export const AuthRoute = withRouter(connect(mapStateToProps, null)(Auth));
+export const ProtectedRoute = withRouter(connect(mapStateToProps, null)(Protected));
