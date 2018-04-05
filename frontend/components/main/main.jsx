02b697d9b6736b1d431e073506a717ce0fe83112
Yu-Jen Chang <juzen2003@gmail.com>
2018-04-04 17:56:05 -0700

    Fine Tune mainpage layout

--- frontend/components/main/main.jsx
+++ frontend/components/main/main.jsx
@@ -4,6 +4,8 @@ import LogInFormContainer from '../session_form/login_container'
 
 class Main extends React.Component {
   render() {
+    const splashMessages = "";
+    const copyrightMessages = "Copyright";
     return (
       <div className="main-page">
         <div className="nav-bar-area">
@@ -14,8 +16,11 @@ class Main extends React.Component {
             <LogInFormContainer />
           </section>
           <section className="section-right">
-            <h3>Right Section</h3>
+            <h3>Keep Track of Your Idea</h3>
           </section>
+        </div>
+        <div className="footer-area">
+          <footer>{copyrightMessages}</footer>
         </div>
       </div>
     )
