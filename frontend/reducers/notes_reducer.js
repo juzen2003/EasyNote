1d83f549174448b4e494afc749e2150ed1dda0da
Yu-Jen Chang <juzen2003@gmail.com>
2018-04-05 16:57:25 -0700

    Add entities & notes reducer

---
+++ frontend/reducers/notes_reducer.js
@@ -0,0 +1,24 @@
+import {
+  RECEIVE_ALL_NOTES,
+  RECEIVE_NOTE,
+  REMOVE_NOTE,
+} from '../actions/note_actions';
+import merge from 'lodash/merge';
+
+const NotesReducer = (oldState={}, action) => {
+  Object.freeze(oldState);
+  switch (action.type) {
+    case RECEIVE_ALL_NOTES:
+      return merge({}, oldState, action.notes);
+    case RECEIVE_NOTE:
+      return merge({}, oldState, {[action.note.id]: action.note});
+    case REMOVE_NOTE:
+      const newState = merge({}, oldState);
+      delete newState[action.noteId];
+      return newState;
+    default:
+      return oldState;
+  }
+};
+
+export default NotesReducer;
