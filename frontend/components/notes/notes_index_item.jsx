15d70b6b9942b759dcee9c9ccabd69b1e451b8fb
Yu-Jen Chang <juzen2003@gmail.com>
2018-04-06 11:09:56 -0700

    Add hover over effect to notes index item

--- frontend/components/notes/notes_index_item.jsx
+++ frontend/components/notes/notes_index_item.jsx
@@ -3,9 +3,11 @@ import React from 'react';
 class NotesIndexItem extends React.Component {
   render() {
     return (
-      <div className="notes-index-item">
-        <h3>{this.props.note.title}</h3>
-        <p>{this.props.note.body}</p>
+      <div className="notes-index-wrapper">
+        <div className="notes-index-item">
+          <h3>{this.props.note.title}</h3>
+          <p>{this.props.note.body}</p>
+        </div>
       </div>
     )
   }
