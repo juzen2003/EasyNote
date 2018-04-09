e6956924a7a71f7f185cac8ce167bcc894803b4e
Yu-Jen Chang <juzen2003@gmail.com>
2018-04-09 11:53:04 -0700

    Remove unecessary onClick handler on Link in NavSideBar

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import NotesReducer from './notes_reducer';
+import NotebooksReducer from './notebooks_reducer';
 
 const entitiesReducer = combineReducers({
-  notes: NotesReducer
+  notes: NotesReducer,
+  notebooks: NotebooksReducer
 });
 
 export default entitiesReducer;
